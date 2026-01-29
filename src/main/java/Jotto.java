497cd4eb3aa3541eb31c1ea3e34a8c904a86fd12
Jaime Serrano Acevedo <xilverscott@gmail.com>
2026-01-29 11:26:31 -0800

    Update on Jotto file with variables and getters and setters on the class

--- src/main/java/Jotto.java
+++ src/main/java/Jotto.java
@@ -1,7 +1,69 @@
 /**
- * @author feng3302
+ * @author jai-serr-ace
  * @version 0.1.0
  * @Since 1/29/26
  **/
+import java.util.ArrayList;
+
 public class Jotto {
+    private int WORD_SIZE;
+    private String currentWord;
+    private int score;
+    private ArrayList<String> playGuess;
+    private ArrayList<String> playWords;
+    private String filename;
+    private ArrayList<String> wordList;
+    private boolean DEBUG;
+
+    public boolean pickWord(){
+
+    }
+
+    public String showWordList(){
+
+    }
+
+    public ArrayList<String> showPlayerGuesses(){
+
+    }
+
+    void playerGuessScores(ArrayList<String> list){
+
+    }
+
+    public void setCurrentWord(String currentWord) {
+        this.currentWord = currentWord;
+    }
+
+    public ArrayList<String> readWords(){
+
+    }
+
+    public void play(){
+
+    }
+
+    void guess(){
+
+    }
+
+    public ArrayList<String> getPlayWords() {
+        return playWords;
+    }
+
+    public String getCurrentWord() {
+        return currentWord;
+    }
+
+    public int getLetterCount() {
+        return WORD_SIZE;
+    }
+
+    public String showPlayedWords(){
+
+    }
+
+    public boolean addPlayerGuess(String guess){
+
+    }
 }
